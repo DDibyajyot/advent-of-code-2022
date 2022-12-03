51969a4e276af45ac1cbaf2563680b7a3cedf366
DDibyajyot <dibyajyotdeepam@gmail.com>
2022-12-03 16:22:58 -0500

    final commit for day 3 both parts :D

--- day-3/day3.py
+++ day-3/day3.py
@@ -24,5 +24,26 @@ def main():
     # print the sum of priorities
     print(sum_of_priorities)
 
+    #//FOR PART TWO//
+
+    
+    #divide the lines into groups of 3
+    groups = [lines[i:i+3] for i in range(0, len(lines), 3)]
+    #initialize the sum of priorities
+    sum_of_priorities_three = 0
+    #for each group
+    for group in groups:
+        #find the common item type and add it only once to the sum
+        for i in group[0]:
+            if i in group[1] and i in group[2]:
+                sum_of_priorities_three += ord(i) - 96 if i.islower() else ord(i) - 38
+                break
+
+
+
+    #print the sum of priorities
+    print(sum_of_priorities_three)
+
+
 if __name__ == "__main__":
     main()
