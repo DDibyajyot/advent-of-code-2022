42eb4b417b6d0b6c779bfde25ea3e574c7ca0b12
DDibyajyot <dibyajyotdeepam@gmail.com>
2022-12-03 11:14:40 -0500

    first commit for day1 part1

---
+++ day-1/day1.py
@@ -0,0 +1,36 @@
+import sys
+
+def main():
+    # read in the file
+    with open(sys.argv[1]) as f:
+        lines = f.readlines()
+
+    # initialize the list of lists
+    lists = [[]]
+
+    # for each line
+    for line in lines:
+        # if the line is blank
+        if line == '\n':
+            # add a new list to the list of lists
+            lists.append([])
+        # otherwise
+        else:
+            # add the line to the last list in the list of lists
+            lists[-1].append(int(line))
+
+    # initialize the max calories
+    max_calories = 0
+
+    # for each list
+    for l in lists:
+        # if the sum of the list is greater than the max calories
+        if sum(l) > max_calories:
+            # set the max calories to the sum of the list
+            max_calories = sum(l)
+
+    # print the max calories
+    print(max_calories)
+
+if __name__ == '__main__':
+    main()
