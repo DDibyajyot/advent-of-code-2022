9439b85cb47301a7286adfb2241b880b34f6566e
DDibyajyot <dibyajyotdeepam@gmail.com>
2022-12-03 11:50:31 -0500

    commit for day2 part one

--- day-2/day2.py
+++ day-2/day2.py
@@ -1,25 +1,3 @@
-# "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
-
-# The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious, so the responses must have been carefully chosen.
-
-# The winner of the whole tournament is the player with the highest score. Your total score is the sum of your scores for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
-
-# Since you can't be sure if the Elf is trying to help you or trick you, you should calculate the score you would get if you were to follow the strategy guide.
-
-# For example, suppose you were given the following strategy guide:
-
-# A Y
-# B X
-# C Z
-# This strategy guide predicts and recommends the following:
-
-# In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
-# In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
-# The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
-# In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
-
-# What would your total score be if everything goes exactly according to your strategy guide?
-
 import sys
 
 def main():
@@ -41,28 +19,22 @@ def main():
 
         # if the first letter is A
         if l[0] == 'A':
-            # if the second letter is X
             if l[1] == 'X':
-                # add 3+1 to the score
                 score += 4
-            # if the second letter is Y
             elif l[1] == 'Y':
-                # add 6+2 to the score
                 score += 8
-            # if the second letter is Z
             elif l[1] == 'Z':
-                # add 0+3 to the score
                 score += 3
 
         # if the first letter is B
         elif l[0] == 'B':
-            # if the second letter is X
             if l[1] == 'X':
                 score += 1
             elif l[1] == 'Y':
                score += 5
             elif l[1] == 'Z':
                 score += 9
+
 
         # if the first letter is C
         elif l[0] == 'C':
@@ -71,7 +43,8 @@ def main():
             elif l[1] == 'Y':
                 score += 2
             elif l[1] == 'Z':
-                score += 9
+                score += 6
+
 
 
     # print the score
