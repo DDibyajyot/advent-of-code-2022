57c73a28dce52c85c627f6925790feab4f58e934
DDibyajyot <dibyajyotdeepam@gmail.com>
2022-12-04 00:36:41 -0500

    final commit for day 4 both parts :D

--- day-4/day4.py
+++ day-4/day4.py
@@ -32,5 +32,39 @@ def main():
     #print the number of pairs that contain each other
     print(pairs_contain_each_other)
 
+    #//FOR PART TWO//
+    #initialize the number of pairs that overlap
+    pairs_overlap = 0
+
+    #for each line
+    for line in lines:
+        #get the two ranges
+        r1 = line.split(",")[0]
+        r2 = line.split(",")[1]
+
+        #get the start and end of each range
+        r1_start = int(r1.split("-")[0])
+        r1_end = int(r1.split("-")[1])
+        r2_start = int(r2.split("-")[0])
+        r2_end = int(r2.split("-")[1])
+
+#use set intersection to solve it
+        #get the intersection of the two ranges
+        intersection = set(range(r1_start, r1_end + 1)).intersection(set(range(r2_start, r2_end + 1)))
+
+        #if the intersection is not empty
+        if intersection:
+            #increment the number of pairs that overlap
+            pairs_overlap += 1
+
+
+
+
+    #print the number of pairs that overlap
+    print(pairs_overlap)
+
 if __name__ == "__main__":
     main()
+
+#well got 2125 for the first star but wasted too much time on the second star doing the exact opposite of what 
+# we were supposed to do so I'm just gonna leave it at that
