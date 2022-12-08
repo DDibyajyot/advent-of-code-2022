a3b30ecae5c603420ce5283ca06e433d81b6f351
DDibyajyot <dibyajyotdeepam@gmail.com>
2022-12-08 09:30:16 -0500

    final commit for day 8 both parts :D

--- day-8/day8.py
+++ day-8/day8.py
@@ -49,7 +49,68 @@ def main():
     print(np.sum(visible))
 
 
-    
+    #//FOR PART TWO//
+
+
+    def scenic_score(row, col):
+        # returns the count of visible trees
+        # looking right, left, up, down
+        score = 1
+        curr_tree = data[row, col]
+        left = data[row, :col] >= curr_tree
+        right = data[row, col + 1:] >= curr_tree
+        up = data[:row, col] >= curr_tree
+        down = data[row + 1:, col] >= curr_tree
+        # left:
+        c = 0
+        for i in range(len(left) - 1, -1, -1):
+            if len(left) < 1:
+                break
+            c += 1
+            if left[i] == True:
+                break
+        score *= c
+        # up:
+        c = 0
+        for i in range(len(up) - 1, -1, -1):
+            if len(up) < 1:
+                break
+            c += 1
+            if up[i] == True:
+                break
+        score *= c
+        # right:
+        c = 0
+        for i in range(len(right)):
+            if len(right) < 1:
+                break
+            c += 1
+            if right[i] == True:
+                break
+        score *= c
+        # down:
+        c = 0
+        for i in range(len(down)):
+            if len(down) < 1:
+                break
+            c += 1
+            if down[i] == True:
+                break
+        score *= c
+        return score
+
+    #array to store the scores
+    scores = np.zeros((rows, cols), dtype=int)
+
+    #loop over the array
+    for row in range(rows):
+        for col in range(cols):
+            scores[row, col] = scenic_score(row, col)
+
+    #for debugging
+    print(scores)
+    #print the max score
+    print(np.max(scores))
 
 
 
